231f3bd9938c7320297fe0e1eeb3508eb1f23e7f
hero1238am <2423467330@qq.com>
2017-12-23 16:02:55 +0800

    拼图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,7 @@ Page({
   createPrizz() {
     let puzzleArr = [];
     for (let i = 1; i < 16; i++) {
-      puzzleArr.push(i);
+      puzzleArr.push(`../../images/${i}.png`);
     };
     puzzleArr = puzzleArr.sort(() => {
       return Math.random();
@@ -19,7 +19,29 @@ Page({
       puzzles: puzzleArr
     });
   },
+  moveFn(e){
+    let {index} = e.target.dataset;
+    let arr = this.data.puzzles;
+    let [curNum, leftNum, rightNum, topNum, bottomNum] = [arr[index],arr[index-1],arr[index+1],arr[index-4],arr[index + 4]];
+    let that = this;
+    function move(n) {
+      arr[index+n] = curNum;
+      arr[index] = '';
+      that.setData({
+        puzzles: arr
+      });
+    }
+    if (leftNum === '' && index !== 4 && index !== 8 && index !== 12) {
+      move(-1);
+    } else if (rightNum === '' && index !== 3 && index !== 7 && index !== 11) {
+      move(1);
+    } else if (topNum === '') {
+      move(-4);
+    } else if (bottomNum === '') {
+      move(4);
+    }
+  },
   onLoad() {
-    this.createPrizz()
+    this.createPrizz();
   }
 })
